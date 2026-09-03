0565a2e11204c60d4f5efba9857d48f623f66777
Safiya <152030388+Safiya-adesanya@users.noreply.github.com>
2026-09-03 19:23:23 +0100

    Add files via upload

---
+++ part01/exericse10_user_profile.py
@@ -0,0 +1,19 @@
+name = "Tim Tester"
+age = 20
+skill1 = "python"
+level1 = "beginner"
+skill2 = "java"
+level2 = "veteran"
+skill3 = "programming"
+level3 = "semiprofessional"
+lower = 2000
+upper = 3000
+
+print(f"my name is {name}, I am {age} years old")
+print()
+print("my skills are")
+print(f' - {skill1} (beginner)')
+print(f' - {skill2} (veteran)') 
+print(f' - {skill3} (semiprofessional)')
+print()
+print(f'I am looking for a job with a salary of {lower}-{upper} euros per month')
