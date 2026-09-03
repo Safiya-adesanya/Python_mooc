7bfc377fb1d156e0f594499949132bf92266c3db
Safiya <152030388+Safiya-adesanya@users.noreply.github.com>
2026-09-03 19:16:54 +0100

    Add files via upload

---
+++ part01/exercise07_user_details.py
@@ -0,0 +1,8 @@
+name = input('what is your name')
+family_name = input('what is your family name')
+street_address = input('what is your address')
+City_post_code = input('what is your city and postal code')
+
+print(name, family_name)
+print(street_address)
+print(City_post_code)
