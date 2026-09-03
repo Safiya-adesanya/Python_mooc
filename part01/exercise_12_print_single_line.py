2f5a874c17972c8ba788017617ebbd7529108c71
Safiya <152030388+Safiya-adesanya@users.noreply.github.com>
2026-09-03 19:28:05 +0100

    Add files via upload

---
+++ part01/exercise_12_print_single_line.py
@@ -0,0 +1,9 @@
+print(5, end="")
+print(" + ",  end="")
+print(8,  end="")
+print(" - ",  end="")
+print(4,  end="")
+print(" = ",  end="")
+print(5 + 8 - 4)
+
+#the programe want 5+8-4=answer in one line
