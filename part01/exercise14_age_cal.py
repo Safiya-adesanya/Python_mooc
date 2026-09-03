486c37548fed96c3ca69edaef207e6bb7db316d9
Safiya <152030388+Safiya-adesanya@users.noreply.github.com>
2026-09-03 19:34:58 +0100

    Add files via upload

---
+++ part01/exercise14_age_cal.py
@@ -0,0 +1,3 @@
+name = input("what is your name? ")
+year_of_birth = int(input("which year were you born? "))
+print(f'Hi {name}, you will be {2021-year_of_birth} years old at the end of the year 2021')
