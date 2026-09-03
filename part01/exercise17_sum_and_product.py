ca0e3d221527017cb58b63f76552ae893e98cad5
Safiya <152030388+Safiya-adesanya@users.noreply.github.com>
2026-09-03 19:42:28 +0100

    Add files via upload

---
+++ part01/exercise17_sum_and_product.py
@@ -0,0 +1,8 @@
+# This program calculates the sum and product of two numbers
+number_1 = int(input("Number 1: "))
+number_2 = int(input("Number 2: "))
+
+product = number_1 * number_2
+
+print(f'The sum of the numbers: {number_1+number_2}')
+print(f'The product of the numbers: {product}')
